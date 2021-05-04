71c7e90182837ac6d0f5fa28ae43d3b20acdc67e
Luke Jones <68298540+sprucemoose22@users.noreply.github.com>
2021-05-04 20:38:06 +0100

    Created components and assets folder
    - Moved App components into components folder
    - Moved logo.svg into assets folder

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+/*import logo from './logo.svg';*/
+import logo from '../../assets/logo.svg';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
