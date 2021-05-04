65c8e384c5fcd1ff30987528bd216353520c7d8b
Luke Jones <68298540+sprucemoose22@users.noreply.github.com>
2021-05-04 20:59:32 +0100

    Created Header component
    - Created base of header component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,12 @@
 /*import logo from './logo.svg';*/
 import logo from '../../assets/logo.svg';
+import Header from '../header';
 import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import './header.scss';
+
+const Header = () => {
+    return (
+        <h1>BT React Code Test - by Luke Jones - 04/05/2021</h1>
+    );
+}
+
+export default Header;
