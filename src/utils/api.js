0f2343558a0085272ebe4e3c0fd6766c5e880edb
Luke Jones <68298540+sprucemoose22@users.noreply.github.com>
2021-05-05 20:00:03 +0100

    News Articles character count can be limited to a configurable number of characters
    - NewsArticles can be configured to display any delimiter after a specified number of characters
    - Added comments to all methods in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,33 @@
 const API_KEY = 'b020f517401c468e959b04ce2f1745c3';
+
 const resultsToDisplay = 10;
 const resultsToDisplayParameter = '&pageSize=' + resultsToDisplay.toString();
 
+const articleCharacterLimit = 150;
+const articleContentDelimiter = '...';
+
+/**
+ * Return a list of articles related to the search query
+ * @param {string} searchQuery The user's search query
+ * @returns Object containing a list of news articles
+ */
 export const getArticles = async(searchQuery) => {
     const url = urlBuilder(searchQuery);
 
     return await fetch(url)
         .then(response => response.json())
         .then(data => {
+            limitArticleCharacterCount(data);
             console.log(data);
         })
         .catch(error => console.log(error));
 }
 
+/**
+ * Creates a valid URL that will return results when fetched
+ * @param {string} searchQuery The user's search query
+ * @returns Encoded URL
+ */
 const urlBuilder = (searchQuery) => {
     return encodeURI(
         'https://newsapi.org/v2/everything?q=' +
@@ -21,4 +36,18 @@ const urlBuilder = (searchQuery) => {
         '&sortBy=popularity&apiKey=' + 
         API_KEY
     );
+}
+
+/**
+ * Limits the number of characters within each news article
+ * @param {JSON} response Response received from News API
+ */
+const limitArticleCharacterCount = (response) => {
+    const articles = response.articles;
+
+    for(const article of articles) {
+        article.content = article.content
+            .substring(0, articleCharacterLimit)
+            .concat(articleContentDelimiter);
+    }
 }
