04aba9d4868d83951e64329b963f54fea1b25774
Luke Jones <68298540+sprucemoose22@users.noreply.github.com>
2021-05-05 18:35:37 +0100

    Began making Header Component Responsive
    - Header component has media queries to handle 2 different screen sizes
    - Header img has alt property

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ import logo from '../../assets/logo.svg';
 const Header = () => {
     return (
         <div className="Header">
-            <img src ={logo} />
+            <img src ={logo} alt="BT Logo"/>
             <h1>BT React Code Test - by Luke Jones - 04/05/2021</h1>
         </div>
     );
