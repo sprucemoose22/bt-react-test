46578e3df359084c2a00a7eb556b0bd5588b8255
Luke Jones <68298540+sprucemoose22@users.noreply.github.com>
2021-05-09 14:03:58 +0100

    Completed mobile design for NewsFeedArticles

--- src/components/news/newsFeed/newsFeedArticle/newsFeedArticle.js
+++ src/components/news/newsFeed/newsFeedArticle/newsFeedArticle.js
@@ -6,9 +6,17 @@ const NewsFeedArticle = (props) => {
     return (
         <div className="NewsFeedArticle">
             <a href={article.url} target='_blank'>
-                <h2>{article.title}</h2>
-                <h3>{article.source.name}</h3>
-                <h4>{article.content}</h4>
+
+                <div className="Title">
+                    <h2>{article.title}</h2>
+                </div>
+
+                <div className="Author">{article.source.name}</div>
+
+                <div className="Content">
+                    <span>{article.content}</span>
+                </div>
+
             </a>
         </div>
     );
