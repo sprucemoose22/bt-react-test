173b6c6266a3121406e07fdca4cbc2df579a8be1
Luke Jones <68298540+sprucemoose22@users.noreply.github.com>
2021-05-09 13:17:53 +0100

    NewsFeedArticle now displays all required information on screen
    - NewsFeedArticle displays:
        - Title
        - Author
        - Content
    - NewsFeedArticle is also a link which will open a new tab and redirect to the article source

--- src/components/news/newsFeed/newsFeedArticle/newsFeedArticle.js
+++ src/components/news/newsFeed/newsFeedArticle/newsFeedArticle.js
@@ -1,9 +1,15 @@
 import './newsFeedArticle.scss';
 
 const NewsFeedArticle = (props) => {
+    const article = props.article;
+
     return (
-        <div className="NewsArticle">
-            <h2>{props.article.title}</h2>
+        <div className="NewsFeedArticle">
+            <a href={article.url} target='_blank'>
+                <h2>{article.title}</h2>
+                <h3>{article.source.name}</h3>
+                <h4>{article.content}</h4>
+            </a>
         </div>
     );
 }
